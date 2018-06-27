111a6a21577c28494a6778069293d44ae2740b75
Cardenas2 <vaniacardenas-21@hotmail.com>
2018-06-26 21:59:07 -0400

    modificar formulario para que se creen los tweet

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,7 +24,30 @@ $(document).ready(function(){
 		var nombrePropio = $ ('#nombre').val();
 		var pensamiento = $ ('#mensaje').val();
 		var enviar = $ ('#borrar').val(); 
-	})  
+		var post=`<div class="tweet__cajpubli">
+					<div class="media">
+						<img class="align-self-end mr-3 tweet__cajpubli-img" src="${imagenUsuario}">
+						<div class="media-body tweet__cajpubli-nombre">
+							<h4 class="mt-5 pt-3 ">${ nombrePopio }</h4>
+							
+						</div>
+					</div>
+					<p class="mt-4 tweet__cajpubli-opinion">
+						${ pensamiento }
+					</p>
+					<div class="row tweet__cajpubli-grupoicon">
+						<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 tweet__cajpubli-icono">
+							<i class="tweet__cajpubli-grupoicon--rojo fas fa-heart"></i>
+						</div>
+						<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 tweet__cajpubli-icono">
+							<span><i class=" fas fa-thumbs-up"></i>1</span>
+						</div>
+						<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 tweet__cajpubli-icono">
+							<button type="reset" class="btn btn-info">Borrar</button>
+						</div>
+					</div><!--fin row tweet__cajpubli-icono-->
+				</div><!--fin tweet__cajpubli-->`
+	});  
 
 
 
